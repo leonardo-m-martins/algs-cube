3ccab54f62c52cfa7f5f39329eb2effe0e1c582d
Leonardo Martins Martins <leonardo.mmartins@outlook.com>
2026-04-19 01:15:02 -0300

    chore: remover algoritmos para grid

--- src/BuscaNP.py
+++ src/BuscaNP.py
@@ -11,41 +11,6 @@ class buscaNP(object):
         for suc in grafo[ind][::ordem]:
             f.append(suc)
         return f
-#--------------------------------------------------------------------------
-# SUCESSORES PARA GRID
-#--------------------------------------------------------------------------
-    def sucessores_grid(self,st,nx,ny,mapa):
-        f = []
-        x, y = st[0], st[1]
-        # DIREITA
-        if y+1<ny:
-            if mapa[x][y+1]==0:
-                suc = []
-                suc.append(x)
-                suc.append(y+1)
-                f.append(suc)
-        # ESQUERDA
-        if y-1>=0:
-            if mapa[x][y-1]==0:
-                suc = []
-                suc.append(x)
-                suc.append(y-1)
-                f.append(suc)
-        # ABAIXO
-        if x+1<nx:
-            if mapa[x+1][y]==0:
-                suc = []
-                suc.append(x+1)
-                suc.append(y)
-                f.append(suc)
-        # ACIMA
-        if x-1>=0:
-            if mapa[x-1][y]==0:
-                suc = []
-                suc.append(x-1)
-                suc.append(y)
-                f.append(suc)
-        return f[::-1]
 #--------------------------------------------------------------------------    
 # EXIBE O CAMINHO ENCONTRADO NA ÁRVORE DE BUSCA (GRAFO e GRID)
 #--------------------------------------------------------------------------    
@@ -121,52 +86,6 @@ class buscaNP(object):
                         return self.exibirCaminho(filho)
         return None
 #--------------------------------------------------------------------------
-# BUSCA EM AMPLITUDE - GRID
-#--------------------------------------------------------------------------
-    def amplitude_grid(self,inicio,fim,nx,ny,mapa):
-        # Finaliza se início for igual a objetivo
-        if inicio == fim:
-            return [inicio]
-
-        # GRID: transforma em tupla
-        t_inicio = tuple(inicio)
-        t_fim = tuple(fim)
-        
-        # Lista para árvore de busca - FILA
-        fila = deque()
-    
-        # Inclui início como nó raíz da árvore de busca
-        raiz = Node(None,t_inicio,0,None,None)
-        fila.append(raiz)
-    
-        # Marca início como visitado
-        visitado = {}
-        visitado[t_inicio] = 0
-        
-        # Executa a busca
-        while fila:
-            # Remove o primeiro da FILA
-            atual = fila.popleft()
-    
-            # Gera sucessores a partir do grid
-            filhos = self.sucessores_grid(atual.estado,nx,ny,mapa)
-    
-            for novo in filhos:
-                t_novo = tuple(novo)
-                flag = True
-                if t_novo in visitado:
-                    if visitado[t_novo]<=atual.v1+1:
-                        flag = False
-                if flag:
-                    filho = Node(atual,novo,atual.v1 + 1,None,None)
-                    fila.append(filho)
-                    visitado[t_novo] = atual.v1 + 1
-                    
-                    # Verifica se encontrou o objetivo - multiobjetivo
-                    if t_novo == t_fim:
-                        return self.exibirCaminho(filho)                            
-        return None
-#--------------------------------------------------------------------------
 # BUSCA EM PROFUNDIDADE - GRAFO
 #--------------------------------------------------------------------------
     def profundidade_grafo(self, inicio, fim, nos, grafo):
@@ -205,51 +124,6 @@ class buscaNP(object):
                     
                     # Verifica se encontrou o objetivo - multiobjetivo
                     if novo == fim:
-                        return self.exibirCaminho(filho)
-        return None
-#--------------------------------------------------------------------------
-# BUSCA EM PROFUNDIDADE - GRID
-#--------------------------------------------------------------------------
-    def profundidade_grid(self,inicio,fim,nx,ny,mapa):
-        # Finaliza se início for igual a objetivo
-        if inicio == fim:
-            return [inicio]
-
-        # GRID: transforma em tupla
-        t_inicio = tuple(inicio)
-        t_fim = tuple(fim)
-        
-        # Lista para árvore de busca - PILHA
-        pilha = deque()
-    
-        # Inclui início como nó raíz da árvore de busca
-        raiz = Node(None,t_inicio,0,None,None)
-        pilha.append(raiz)
-    
-        # Marca início como visitado
-        visitado = {}
-        visitado[t_inicio] = 0
-        
-        while pilha:
-            # Remove o último da PILHA
-            atual = pilha.pop()
-          
-            # Gera sucessores a partir do grid
-            filhos = self.sucessores_grid(atual.estado,nx,ny,mapa) # grid
-    
-            for novo in filhos:
-                t_novo = tuple(novo)
-                flag = True
-                if t_novo in visitado:
-                    if visitado[t_novo]<=atual.v1+1:
-                        flag = False
-                if flag:
-                    filho = Node(atual,novo,atual.v1 + 1,None,None)
-                    pilha.append(filho)
-                    visitado[t_novo] = atual.v1 + 1
-                    
-                    # Verifica se encontrou o objetivo - multiobjetivo
-                    if t_novo == t_fim:
                         return self.exibirCaminho(filho)
         return None
 #--------------------------------------------------------------------------
@@ -295,52 +169,6 @@ class buscaNP(object):
                             return self.exibirCaminho(filho)
         return None
 #--------------------------------------------------------------------------
-# BUSCA EM PROFUNDIDADE LIMITADA - GRID
-#--------------------------------------------------------------------------
-    def prof_limitada_grid(self,inicio,fim,nx,ny,mapa,lim):
-        # Finaliza se início for igual a objetivo
-        if inicio == fim:
-            return [inicio]
-    
-        # GRID: transforma em tupla
-        t_inicio = tuple(inicio)
-        t_fim = tuple(fim)
-        
-        # Lista para árvore de busca - PILHA
-        pilha = deque()
-    
-        # Inclui início como nó raíz da árvore de busca
-        raiz = Node(None,t_inicio,0,None,None)
-        pilha.append(raiz)
-    
-        # Marca início como visitado
-        visitado = {}
-        visitado[t_inicio] = 0
-        
-        while pilha:
-            # Remove o último da PILHA
-            atual = pilha.pop()
-            
-            if atual.v1<lim:
-                # Gera sucessores a partir do grid
-                filhos = self.sucessores_grid(atual.estado,nx,ny,mapa)
-        
-                for novo in filhos:
-                    t_novo = tuple(novo)
-                    flag = True
-                    if t_novo in visitado:
-                        if visitado[t_novo]<=atual.v1+1:
-                            flag = False
-                    if flag:
-                        filho = Node(atual,novo,atual.v1 + 1,None,None)
-                        pilha.append(filho)
-                        visitado[t_novo] = atual.v1 + 1
-                        
-                        # Verifica se encontrou o objetivo - multiobjetivo
-                        if t_novo == t_fim:
-                            return self.exibirCaminho(filho)
-        return None
-#--------------------------------------------------------------------------
 # BUSCA EM APROFUNDAMENTO ITERATIVO - GRAFO
 #--------------------------------------------------------------------------
     def aprof_iterativo_grafo(self,inicio,fim,nos,grafo,lim_max):
@@ -382,55 +210,6 @@ class buscaNP(object):
                             # Verifica se encontrou o objetivo
                             if novo == fim:
                                 return self.exibirCaminho(filho)
-        return None
-#--------------------------------------------------------------------------
-# BUSCA EM APROFUNDAMENTO ITERATIVO - grid
-#--------------------------------------------------------------------------
-    def aprof_iterativo_grid(self,inicio,fim,nx,ny,mapa,lim_max):
-        # Finaliza se início for igual a objetivo
-        if inicio == fim:
-            return [inicio]
-        
-        for lim in range(1,lim_max):   
-            # GRID: transforma em tupla
-            t_inicio = tuple(inicio)
-            t_fim = tuple(fim)
-            
-            # Lista para árvore de busca - FILA
-            pilha = deque()
-        
-            # Inclui início como nó raíz da árvore de busca
-            raiz = Node(None,t_inicio,0,None,None)
-            pilha.append(raiz)
-        
-            # Marca início como visitado
-            visitado = {}
-            visitado[t_inicio] = 0
-            
-            while pilha:
-                # Remove o primeiro da FILA
-                atual = pilha.pop()
-                
-                if atual.v1<lim:
-                    # Gera sucessores a partir do grid
-                    filhos = self.sucessores_grid(atual.estado,nx,ny,mapa)
-            
-                    for novo in filhos:
-                        t_novo = tuple(novo)
-                        flag = True
-                        if t_novo in visitado:
-                            if visitado[t_novo]<=atual.v1+1:
-                                flag = False
-                        if flag:
-                            filho = Node(atual,novo,atual.v1 + 1,None,None)
-                            pilha.append(filho)
-                            visitado[t_novo] = atual.v1 + 1
-                            
-                            # Verifica se encontrou o objetivo
-                            if t_novo == t_fim:
-                                return self.exibirCaminho(filho)
-            visitado.clear()
-            pilha.clear()
         return None
 #--------------------------------------------------------------------------
 # BUSCA BIDIRECIONAL - GRAFO
@@ -507,85 +286,4 @@ class buscaNP(object):
                         if novo in visitado1:
                             return self.exibirCaminho_bid(novo,fila1,fila2)
                         
-        return None
-#--------------------------------------------------------------------------
-# BUSCA BIDIRECIONAL - GRID
-#--------------------------------------------------------------------------
-    def bidirecional_grid(self,inicio,fim,nx,ny,mapa):
-        if inicio == fim:
-            return [inicio]
-        # GRID: transforma em tupla
-        t_inicio = tuple(inicio)
-        t_fim = tuple(fim)
-
-        # Lista para árvore de busca a partir da origem - FILA
-        fila1 = deque()
-        
-        # Lista para árvore de busca a partir do destino - FILA
-        fila2 = deque()
-        
-        # Inclui início e fim como nó raíz da árvore de busca
-        raiz = Node(None,t_inicio,0,None,None)
-        fila1.append(raiz)
-        raiz = Node(None,t_fim,0,None,None)
-        fila2.append(raiz)
-    
-        # Visitados mapeando estado -> Node (para reconstruir o caminho)
-        visitado1 = {}
-        visitado1[t_inicio] = 0
-        visitado2 = {}
-        visitado2[t_fim] = 0
-        print(visitado1,visitado2)
-        
-        nivel = 0
-        while fila1 and fila2:
-            # ****** Executa AMPLITUDE a partir da ORIGEM *******
-            # Quantidade de nós no nível atual
-            nivel = len(fila1)  
-            for _ in range(nivel):
-                # Remove o primeiro da FILA
-                atual = fila1.popleft()
-                
-                # Gera sucessores a partir do grid
-                filhos = self.sucessores_grid(atual.estado,nx,ny,mapa) # grid
-
-                for novo in filhos:
-                    t_novo = tuple(novo)
-                    flag = True
-                    if t_novo in visitado1:
-                        if visitado1[t_novo]<=atual.v1+1:
-                            flag = False
-                    if flag:
-                        filho = Node(atual,novo,atual.v1 + 1,None,None)
-                        fila1.append(filho)
-                        visitado1[t_novo] = atual.v1 + 1
-
-                        # Encontrou encontro com a outra AMPLITUDE
-                        if t_novo in visitado2:
-                            return self.exibirCaminho_bid(novo,fila1,fila2)
-            
-            # ****** Executa AMPLITUDE a partir do OBJETIVO *******
-            # Quantidade de nós no nível atual
-            nivel = len(fila2)  
-            for _ in range(nivel):
-                # Remove o primeiro da FILA
-                atual = fila2.popleft()
-                
-                # Gera sucessores a partir do grid
-                filhos = self.sucessores_grid(atual.estado,nx,ny,mapa)
-
-                for novo in filhos:
-                    t_novo = tuple(novo)
-                    flag = True
-                    if t_novo in visitado2:
-                        if visitado2[t_novo]<=atual.v1+1:
-                            flag = False
-                    if flag:
-                        filho = Node(atual,novo,atual.v1 + 1,None,None)
-                        fila2.append(filho)
-                        visitado2[t_novo] = atual.v1 + 1
-
-                        # Encontrou encontro com a outra AMPLITUDE
-                        if t_novo in visitado1:
-                            return self.exibirCaminho_bid(novo,fila1,fila2)
         return None
--- src/BuscaP.py
+++ src/BuscaP.py
@@ -12,57 +12,6 @@ class buscaP(object):
         for suc in grafo[ind][::ordem]:
             f.append(suc)
         return f
-#--------------------------------------------------------------------------
-# SUCESSORES PARA GRID
-#--------------------------------------------------------------------------
-    def sucessores_grid(self,st,nx,ny,mapa):
-        f = []
-        x, y = st[0], st[1]
-        # DIREITA
-        if y+1<ny:
-            if mapa[x][y+1]==0:
-                suc = []
-                suc.append(x)
-                suc.append(y+1)
-                custo = 5
-                aux = []
-                aux.append(suc)
-                aux.append(custo)
-                f.append(aux)
-        # ESQUERDA
-        if y-1>=0:
-            if mapa[x][y-1]==0:
-                suc = []
-                suc.append(x)
-                suc.append(y-1)
-                custo = 7
-                aux = []
-                aux.append(suc)
-                aux.append(custo)
-                f.append(aux)
-        # ABAIXO
-        if x+1<nx:
-            if mapa[x+1][y]==0:
-                suc = []
-                suc.append(x+1)
-                suc.append(y)
-                custo = 2
-                aux = []
-                aux.append(suc)
-                aux.append(custo)
-                f.append(aux)
-        # ACIMA
-        if x-1>=0:
-            if mapa[x-1][y]==0:
-                suc = []
-                suc.append(x-1)
-                suc.append(y)
-                custo = 3
-                aux = []
-                aux.append(suc)
-                aux.append(custo)
-                f.append(aux)        
-        return f
 #--------------------------------------------------------------------------    
 # INSERE NA LISTA MANTENDO-A ORDENADA
 #--------------------------------------------------------------------------    
@@ -111,64 +60,7 @@ class buscaP(object):
              ]
         i_n = nos.index(n)
         i_destino = nos.index(destino)
-        return h[i_destino][i_n]     
-#--------------------------------------------------------------------------    
-# GERA H - GRID
-#--------------------------------------------------------------------------    
-    def heuristica_grid(self,p1,p2):
-        if (p2[0]-p1[0])<0:
-            c1 = 3
-        else:
-            c1 = 2
-        if (p2[1]-p1[1])<0:
-            c2 = 7
-        else:
-            c2 = 5
-        h = sqrt(c1*(p1[0]-p2[0])*(p1[0]-p2[0]) + c2*(p1[1]-p2[1])*(p1[1]-p2[1]))
-        #h = c1*fabs(p1[0]-p2[0]) + c2*fabs(p1[1]-p2[1])
-        return h
-# -----------------------------------------------------------------------------
-# CUSTO UNIFORME - GRID
-# -----------------------------------------------------------------------------
-    def custo_uniforme_grid(self,inicio,fim,mapa,nx,ny): # grid
-        # Origem igual a destino
-        if inicio == fim:
-            return [inicio], 0
-        
-        # Fila de prioridade baseada em deque + inserção ordenada
-        lista = deque()
-        t_inicio = tuple(inicio)
-        raiz = NodeP(None, t_inicio,0, None, None, 0)
-        lista.append(raiz)
-    
-        # Controle de nós visitados
-        visitado = {tuple(inicio): raiz}
-        
-        # loop de busca
-        while lista:
-            # remove o primeiro nó
-            atual = lista.popleft()
-            valor_atual = atual.v2
-    
-            # Chegou ao objetivo
-            if atual.estado == fim:
-                return self.exibirCaminho(atual), atual.v2
-    
-            # Gera sucessores - grid
-            filhos = self.sucessores_grid(atual.estado,nx,ny,mapa)
-    
-            for novo in filhos:
-                # custo acumulado até o sucessor
-                v2 = valor_atual + novo[1]
-                v1 = v2
-    
-                # Não visitado ou custo melhor
-                t_novo = tuple(novo[0])
-                if (t_novo not in visitado) or (v2<visitado[t_novo].v2):
-                    filho = NodeP(atual,t_novo, v1, None, None, v2)
-                    visitado[t_novo] = filho
-                    self.inserir_ordenado(lista, filho)
-        return None
+        return h[i_destino][i_n]
 # -----------------------------------------------------------------------------
 # CUSTO UNIFORME - GRAFO
 # -----------------------------------------------------------------------------
@@ -211,49 +103,7 @@ class buscaP(object):
                     self.inserir_ordenado(lista, filho)
         return None
 # -----------------------------------------------------------------------------
-# GREEDY - GRID
-# -----------------------------------------------------------------------------
-    def greedy_grid(self,inicio,fim,mapa,nx,ny): # grid
-        # Origem igual a destino
-        if inicio == fim:
-            return [inicio], 0
-        
-        # Fila de prioridade baseada em deque + inserção ordenada
-        lista = deque()
-        t_inicio = tuple(inicio)
-        raiz = NodeP(None, t_inicio,0, None, None, 0)
-        lista.append(raiz)
-    
-        # Controle de nós visitados
-        visitado = {tuple(inicio): raiz}
-        
-        # loop de busca
-        while lista:
-            # remove o primeiro nó
-            atual = lista.popleft()
-            valor_atual = atual.v2
-    
-            # Chegou ao objetivo
-            if atual.estado == fim:
-                return self.exibirCaminho(atual), atual.v2
-            
-            # Gera sucessores
-            filhos = self.sucessores_grid(atual.estado,nx,ny,mapa)
-    
-            for novo in filhos:
-                # custo acumulado até o sucessor
-                v2 = valor_atual + novo[1]
-                v1 = self.heuristica_grid(novo[0],fim)  
-    
-                # Não visitado ou custo melhor
-                t_novo = tuple(novo[0])
-                if (t_novo not in visitado) or (v2<visitado[t_novo].v2):
-                    filho = NodeP(atual,t_novo, v1, None, None, v2)
-                    visitado[t_novo] = filho
-                    self.inserir_ordenado(lista, filho)
-        return None
-# -----------------------------------------------------------------------------
-# GREEDY - GRID
+# GREEDY - GRAFO
 # -----------------------------------------------------------------------------
     def greedy_grafo(self,inicio,fim,nos,grafo):
         # Origem igual a destino
@@ -294,49 +144,7 @@ class buscaP(object):
                     self.inserir_ordenado(lista, filho)
         return None
 # -----------------------------------------------------------------------------
-# A ESTRELA- GRID
-# -----------------------------------------------------------------------------
-    def a_estrela_grid(self,inicio,fim,mapa,nx,ny):
-        # Origem igual a destino
-        if inicio == fim:
-            return [inicio], 0
-        
-        # Fila de prioridade baseada em deque + inserção ordenada
-        lista = deque()
-        t_inicio = tuple(inicio)
-        raiz = NodeP(None, t_inicio,0, None, None, 0)
-        lista.append(raiz)
-    
-        # Controle de nós visitados
-        visitado = {tuple(inicio): raiz}
-        
-        # loop de busca
-        while lista:
-            # remove o primeiro nó
-            atual = lista.popleft()
-            valor_atual = atual.v2
-    
-            # Chegou ao objetivo
-            if atual.estado == fim:
-                return self.exibirCaminho(atual), atual.v2
-            
-            # Gera sucessores
-            filhos = self.sucessores_grid(atual.estado,nx,ny,mapa)
-    
-            for novo in filhos:
-                # custo acumulado até o sucessor
-                v2 = valor_atual + novo[1]
-                v1 = v2 + self.heuristica_grid(novo[0],fim)  
-    
-                # Não visitado ou custo melhor
-                t_novo = tuple(novo[0])
-                if (t_novo not in visitado) or (v2<visitado[t_novo].v2):
-                    filho = NodeP(atual,t_novo, v1, None, None, v2)
-                    visitado[t_novo] = filho
-                    self.inserir_ordenado(lista, filho)
-        return None
-# -----------------------------------------------------------------------------
-# AIA ESTRELA - GRAFO
+# A ESTRELA - GRAFO
 # -----------------------------------------------------------------------------
     def a_estrela_grafo(self,inicio,fim,nos,grafo):
         # Origem igual a destino
@@ -377,59 +185,7 @@ class buscaP(object):
                     self.inserir_ordenado(lista, filho)
         return None
 # -----------------------------------------------------------------------------
-# AIA ESTRELA- GRID
-# -----------------------------------------------------------------------------
-    def aia_estrela_grid(self,inicio,fim,mapa,nx,ny):
-        # Origem igual a destino
-        if inicio == fim:
-            return [inicio], 0
-        lim = self.heuristica_grid(inicio,fim)
-        
-        while True:        
-            # Fila de prioridade baseada em deque + inserção ordenada
-            lista = deque()
-            t_inicio = tuple(inicio)
-            raiz = NodeP(None, t_inicio,0, None, None, 0)
-            lista.append(raiz)
-        
-            # Controle de nós visitados
-            visitado = {tuple(inicio): raiz}
-            
-            # loop de busca
-            novo_lim = []
-            while lista:
-                # remove o primeiro nó
-                atual = lista.popleft()
-                valor_atual = atual.v2
-        
-                # Chegou ao objetivo
-                if atual.estado == fim:
-                    return self.exibirCaminho(atual), atual.v2
-                
-                # Gera sucessores
-                filhos = self.sucessores_grid(atual.estado,nx,ny,mapa)
-        
-                for novo in filhos:
-                    # custo acumulado até o sucessor
-                    v2 = valor_atual + novo[1]
-                    v1 = v2 + self.heuristica_grid(novo[0],fim)
-                    
-                    if v1<=lim:        
-                        # Não visitado ou custo melhor
-                        t_novo = tuple(novo[0])
-                        if (t_novo not in visitado) or (v2<visitado[t_novo].v2):
-                            filho = NodeP(atual,t_novo, v1, None, None, v2)
-                            visitado[t_novo] = filho
-                            self.inserir_ordenado(lista, filho)
-                    else:
-                        novo_lim.append(v1)
-            lim = (int)(sum(novo_lim)/(len(novo_lim)))
-            lista.clear()
-            visitado.clear()
-            novo_lim.clear()
-        return None
-# -----------------------------------------------------------------------------
-# A ESTRELA - GRAFO
+# AIA ESTRELA - GRAFO
 # -----------------------------------------------------------------------------
     def aia_estrela_grafo(self,inicio,fim,nos,grafo):
         lim = self.heuristica_grafo(nos,inicio,fim)
