75d9c27cab3ca35dd3a2f0e73ad4e32a7ee3f236
Leonardo Martins Martins <leonardo.mmartins@outlook.com>
2026-04-20 19:52:16 -0300

    perf: otimizada profundidade limitada com arrays e numba

--- src/BuscaNP.py
+++ src/BuscaNP.py
@@ -16,6 +16,7 @@ def exibir_caminho(inicio,fim,pai):
         caminho.append(atual)
         atual = pai[atual]
     caminho.append(atual)
+    caminho.reverse()
     return caminho
 #--------------------------------------------------------------------------
 # BUSCA EM AMPLITUDE - GRAFO
@@ -58,7 +59,7 @@ def amplitude_grafo(inicio,fim,grafo):
         
         head += 1
     
-    raise ValueError("Nenhum caminho entre inicio e fim")
+    return typed.List.empty_list(uint32)
 #--------------------------------------------------------------------------
 # BUSCA EM PROFUNDIDADE - GRAFO
 #--------------------------------------------------------------------------
@@ -99,7 +100,49 @@ def profundidade_grafo(inicio, fim, grafo):
             if novo == fim:
                 return exibir_caminho(inicio,fim,pai)
 
-    raise ValueError("Nenhum caminho entre inicio e fim")
+    return typed.List.empty_list(uint32)
+#--------------------------------------------------------------------------
+# BUSCA EM PROFUNDIDADE LIMITADA - GRAFO
+#--------------------------------------------------------------------------
+@njit(caminho_type(uint32, uint32, uint32[:, ::1], uint32))
+def prof_limitada_grafo(inicio,fim,grafo,lim):
+    if inicio == fim:
+        caminho = typed.List.empty_list(types.uint32)
+        caminho.append(inicio)
+        return caminho
+    
+    pilha = np.empty(len(grafo), dtype=np.uint32)
+    pilha[0] = inicio
+    top = 0
+
+    pai = np.empty(len(grafo), dtype=np.uint32)
+
+    visitado = np.full(len(grafo), -1, dtype=np.int32)
+    visitado[inicio] = 0
+
+    while top >= 0:
+        # retira da pilha
+        atual = pilha[top]
+        top -= 1
+        filhos = grafo[atual]
+
+        if visitado[atual] >= lim: continue
+
+        for novo in filhos:
+            # verifica se foi visitado
+            if visitado[novo] != -1 and visitado[novo] <= visitado[atual] + 1: continue
+            # marca como visitado (com valor = v1)
+            visitado[novo] = visitado[atual] + 1
+            # adiciona à pilha
+            top += 1
+            pilha[top] = novo
+            # guarda o pai de "novo"
+            pai[novo] = atual
+            # verifica se é o objetivo
+            if novo == fim:
+                return exibir_caminho(inicio,fim,pai)
+
+    return typed.List.empty_list(uint32)
 
 class buscaNP(object):
 #--------------------------------------------------------------------------
